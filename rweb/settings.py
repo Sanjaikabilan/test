0c4621821bb8a7ca2629e33bbd3adee35c750304
Sanjaikabilan <83118883+Sanjaikabilan@users.noreply.github.com>
2023-03-24 12:39:10 +0000

    db chnages

--- rweb/settings.py
+++ rweb/settings.py
@@ -14,6 +14,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,12 +140,13 @@ WSGI_APPLICATION = "rweb.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#get the database url from the environment variable
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
+
+
 
 
 # Password validation
