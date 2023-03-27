aae1fe06b4f1013bdab5d094476d4866677a4bfa
Sanjaikabilan <sanjaikabilan.20mc@kct.ac.in>
2023-03-27 19:01:50 +0530

    this is it

--- rweb/settings.py
+++ rweb/settings.py
@@ -228,7 +228,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
